2a6f29ebeab2d85ae2af522aa1612609c87b2e98
Sean <c21443404@mytudublin.ie>
2023-12-13 01:26:30 +0100

    Merge changes from remote repository again

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
-from flask import request
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS
 import json
+
 mysql = MySQL()
 app = Flask(__name__)
 CORS(app)
-# My SQL Instance configurations
+
+# MySQL Instance configurations
 # Change these details to match your instance configurations
 app.config['MYSQL_USER'] = 'A'
 app.config['MYSQL_PASSWORD'] = 'B'
@@ -14,36 +15,33 @@ app.config['MYSQL_DB'] = 'student'
 app.config['MYSQL_HOST'] = 'db.mydomain.ie'
 mysql.init_app(app)
 
-@app.route("/add") #Add Student
+@app.route("/add", methods=['POST'])  # Change to POST method for adding a student
 def add():
-  name = request.args.get('name')
-  email = request.args.get('email')
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email) # kludge - use stored proc or params
-  cur.execute(s)
-  mysql.connection.commit()
+    data = request.get_json()
+    name = data.get('name')
+    email = data.get('email')
+    cur = mysql.connection.cursor()
+    cur.execute('INSERT INTO students(studentName, email) VALUES (%s, %s)', (name, email))
+    mysql.connection.commit()
+    return jsonify({"Result": "Success"})
 
-  return '{"Result":"Success"}' # Really? maybe we should check!
-  
-@app.route("/") #Default - Show Data
-def read(): # Name of the method
-  cur = mysql.connection.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM students''') # execute an SQL statment
-  rv = cur.fetchall() #Retreive all rows returend by the SQL statment
-  Results=[]
-  for row in rv: #Format the Output Results and add to return string
-    Result={}
-    Result['Name']=row[0].replace('\n',' ')
-    Result['Email']=row[1]
-    Result['ID']=row[2]
-    Results.append(Result)
-  response={'Results':Results, 'count':len(Results)}
-  ret=app.response_class(
-    response=json.dumps(response),
-    status=200,
-    mimetype='application/json'
-  )
-  return ret #Return the data in a string format
+@app.route("/", methods=['GET'])  # Changed route for better RESTful practices
+def read():
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM students')
+    rv = cur.fetchall()
+    results = []
+    for row in rv:
+        result = {
+            'Name': row[1],
+            'Email': row[2],
+            'ID': row[0]
+        }
+        results.append(result)
+    response = {'Results': results, 'count': len(results)}
+    return jsonify(response)
+
 if __name__ == "__main__":
-  app.run(host='0.0.0.0',port='8080') #Run the flask app at port 8080
+    app.run(host='0.0.0.0', port=8080)
+
 
--- db.sql
+++ db.sql
@@ -1,12 +1,23 @@
+-- Create user and grant privileges
 CREATE USER 'A'@'%' IDENTIFIED BY 'B';
-GRANT ALL PRIVILEGES ON *.* to 'A'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'A'@'%';
 
-CREATE DATABASE student; 
+-- Create and use the database
+CREATE DATABASE IF NOT EXISTS student;
 USE student;
-CREATE TABLE students (studentName VARCHAR(255), email VARCHAR(255), studentID INT NOT NULL AUTO_INCREMENT,
-PRIMARY KEY(studentID));
 
-INSERT INTO students (studentName, email) values ("first student", "firststudent@mydomain.ie"); 
-INSERT INTO students (studentName, email) values ("second student", "secondstudent@mydomain.ie ");
+-- Create students table
+CREATE TABLE IF NOT EXISTS students (
+    studentID INT AUTO_INCREMENT PRIMARY KEY,
+    studentName VARCHAR(255),
+    email VARCHAR(255)
+);
+
+-- Insert sample data
+INSERT INTO students (studentName, email) VALUES 
+    ("first student", "firststudent@mydomain.ie"),
+    ("second student", "secondstudent@mydomain.ie");
+
+-- Select data from students table
 SELECT * FROM students;
-exit;
+
